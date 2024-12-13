6dce9e78554d3af599aef5ad0e4d93610ab7230a
안동환 <ddh1713flrwls1713@gmail.com>
2024-12-13 18:35:47 +0900

    [ADD] 다형성1

---
+++ src/main/java/com/sparta/javaintroduction/poly/Child.java
@@ -0,0 +1,8 @@
+package com.sparta.javaintroduction.poly;
+
+public class Child extends Parent {
+
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
---
+++ src/main/java/com/sparta/javaintroduction/poly/Parent.java
@@ -0,0 +1,8 @@
+package com.sparta.javaintroduction.poly;
+
+public class Parent {
+
+    public void parentMethod() {
+        System.out.println("Parent.parentMethod");
+    }
+}
